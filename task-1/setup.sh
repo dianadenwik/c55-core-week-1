9ea027f99b9236565557b39ea2d0639d0a6a01dd
Diana Chukhrai <diana.chukhrai31@gmail.com>
2026-01-14 12:27:15 +0100

    Finish

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -12,3 +12,15 @@ touch resources/family_picture.jpg
 touch resources/icon.png
 touch resources/logo.png
 touch src/program.java
+echo "Setup project..."
+echo "Welcome to my project" > README.md
+rm -r src/profile
+rm resources/family_picture.jpg
+echo "Setup javascript…"
+mv src/program.java src/program.js
+echo "console.log('JavaScript works!');" > src/program.js
+node src/program.js
+git add .
+git commit -m "third commit"
+ls ~
+echo "All done"
