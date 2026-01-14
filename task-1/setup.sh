697da31a9458bad132116fb6d9e5c2905f0c6c02
Diana Chukhrai <diana.chukhrai31@gmail.com>
2026-01-14 11:46:53 +0100

    initial commit

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,14 @@
-# Write your code here
+# Creating project...
+mkdir project
+cd project
+git init
+touch README.md
+touch settings.conf
+mkdir resources
+mkdir src
+mkdir src/database
+mkdir src/profile
+touch resources/family_picture.jpg
+touch resources/icon.png
+touch resources/logo.png
+touch src/program.java
